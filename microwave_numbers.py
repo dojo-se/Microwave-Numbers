0e38ec420830bfc8f5c76da0ded1bdabbe2635d6
dojo-se <dojo-se@wagner-laptop.(none)>
2011-02-03 20:59:30 -0300

    Esboco geral do programa (Luiz Eduardo)

--- microwave_numbers.py
+++ microwave_numbers.py
@@ -1,6 +1,25 @@
 import unittest
 
+KEYBOARD = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (None, 0, '*'))
+
+def position(number)
+    return None
+    
+def steps_between(postion_initial, position_final)
+    return None
+    
 def microwave_numbers(seconds):
+    s_distance = []
+    for i in str(seconds)
+        s_distance.append(position(i))
+    
+    minutes = seconds / 60
+    seconds = seconds % 60
+    minutes = minutes * 100
+    minutes = minutes+seconds
+    m_distance = []
+    for i in str(minutes):
+        m_distance.append(position(i))
     return 99
 
 class TestMicroWaveNumbers(unittest.TestCase):
