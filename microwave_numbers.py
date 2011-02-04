f5d79903314feefcfd99e31cc808ff0b7b837f9d
dojo-se <dojo-se@wagner-laptop.(none)>
2011-02-03 21:13:03 -0300

    Implementa posicionador das teclas (Davi)

--- microwave_numbers.py
+++ microwave_numbers.py
@@ -2,24 +2,28 @@ import unittest
 
 KEYBOARD = ((1, 2, 3), (4, 5, 6), (7, 8, 9), (None, 0, '*'))
 
-def position(number)
-    return None
+def position(number):
+    for index, tuple_ in enumerate(KEYBOARD):
+        try:
+            return (index, tuple_.index(number))
+        except ValueError:
+            pass
     
-def steps_between(postion_initial, position_final)
+def steps_between(position_initial, position_final):
     return None
     
 def microwave_numbers(seconds):
     s_distance = []
-    for i in str(seconds)
+    for i in str(seconds):
         s_distance.append(position(i))
     
-    minutes = seconds / 60
-    seconds = seconds % 60
+    minutes, seconds = divmod(seconds, 60)
     minutes = minutes * 100
-    minutes = minutes+seconds
+    minutes = minutes + seconds
     m_distance = []
     for i in str(minutes):
         m_distance.append(position(i))
+    print s_distance, m_distance
     return 99
 
 class TestMicroWaveNumbers(unittest.TestCase):
